1ef217e56584bca9e52e888b4906a2432ed1a3bb
Hasan Yahya <hasna786yahya@gmail.com>
2024-07-01 00:18:55 +0500

    refactor: Add lazy loading for images in Playground component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import { Playground } from './components/component/playground';
+import Apology from './components/component/apology';
+
+const isDesktop = () => {
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    const isMobile = /mobile|android|iphone|ipad|tablet|touch|samsung|fridge/i.test(userAgent);
+    return !isMobile;
+};
 
 function App() {
-    return (
-        <Playground />
+    if (isDesktop()) {
+        return (
+            <Playground />
+        )
+    }
+    else {
+        return (
+            <Apology />
 
-    )
+        )
+    }
 }
 
 export default App
---
+++ src/components/component/apology.jsx
@@ -0,0 +1,8 @@
+export default function Apology() {
+    return (
+        <div>
+            <img src="../../../public/sorry.svg" />
+            <h1>Sorry! This app is only available on desktop.</h1>
+        </div>
+    );
+}
